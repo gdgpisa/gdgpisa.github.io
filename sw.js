618671b935fee700035b314c6c2898bf4ca95dfb
domenicoblanco <doddo97@me.com>
2017-10-21 01:34:02 +0200

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,45 @@
+var cacheName = 'gdgpisa-cache-v1';
+var urlsToCache = [
+    '/',      
+    '/blog/',
+    '/feedbackform/',
+    '/blog/',
+    '/category/jekyll',
+    '/category/others',
+    '/io16/plugin/markdown/example.html',
+    '/io16/css/theme/',
+    // can be automated rather than manual entries
+    "/assets/header.jpg",
+    "/static/img/avatar.png",
+    "/static/img/logo.png",
+    "/static/css/main.css",
+    "/static/css/bootstrap-material-design.min.css",
+    "/static/css/bootstrap.min.css",
+    "/about.md",
+    "/blog.html"
+];
+
+self.addEventListener('install', function(event) {
+  // Perform install steps
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(function(cache) {
+        console.log('Opened cache');
+        return cache.addAll(urlsToCache);
+      })
+  );
+});
+
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    caches.match(event.request)
+      .then(function(response) {
+        // Cache hit - return response
+        if (response) {
+          return response;
+        }
+        return fetch(event.request);
+      }
+    )
+  );
+});
