ca9b8c37ae5618fd50eac9324a96e7837b661a63
domenicoblanco <doddo97@me.com>
2017-10-24 17:02:48 +0200

    Code cleaning

--- static/js/web-application.js
+++ static/js/web-application.js
@@ -1,30 +1,20 @@
 function showOfflineToast() {
- let offlineToast = document.querySelector('.offline-ready');
-  offlineToast.classList.add('active');
-  setTimeout(function(){ 
-    offlineToast.className = offlineToast.className.replace("active", "").trim();
-  }, 5500);
-}
- 
-// (1)
-if (navigator.serviceWorker) {
-  navigator.serviceWorker.register('/sw.js').then(function(reg) {
-      if (!reg.installing) return;
-      console.log("[*] ServiceWorker is installing...");
- 
-      var worker = reg.installing;
- 
-      worker.addEventListener('statechange', function() {
-          if (worker.state == 'redundant') {
-              console.log('[*] Install failed');
-          }
-          if (worker.state == 'installed') {
-              console.log('[*] Install successful!');
-          }
-          // (2)
-          if (worker.state == 'activated' && !navigator.serviceWorker.controller) {
-            showOfflineToast();
-          }
-      });
-  });
+	if (navigator.serviceWorker) {
+  		navigator.serviceWorker.register('/sw.js').then(function(reg) {
+      		if (!reg.installing) return;
+      		console.log("[*] ServiceWorker is installing...");
+       		var worker = reg.installing;
+ 			worker.addEventListener('statechange', function() {
+          		if (worker.state == 'redundant') {
+              		console.log('[*] Install failed');
+          		}
+          		if (worker.state == 'installed') {
+              		console.log('[*] Install successful!');
+          		}
+          		if (worker.state == 'activated' && !navigator.serviceWorker.controller) {
+            		showOfflineToast();
+          		}
+      		});
+  		});
+	}
 }
