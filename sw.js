4121d126f1d68496eeaef074cb82a75fbf680701
domenicoblanco <doddo97@me.com>
2017-10-25 08:14:28 +0200

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,20 +1,21 @@
 var cacheName = 'gdgpisa-cache-v1.2';
-var urlsToCache = [
-    '/',
-    '/index.html',      
-    '/blog/',
-    '/manifests/manifest_webapp.json',
-    '/manifests/manifest_notpush.json',
-    '/assets/header.jpg',
-    '/static/img/avatar.png',
-    '/static/img/logo.png',
-    '/static/css/main.css',
-    '/static/css/bootstrap-material-design.min.css',
-    '/static/css/bootstrap.min.css',
-    '/static/css/syntax.css',
-    '/static/css/thickbox.css',
-    '/static/css/projects.css'
-];
+var urlsToCache = [];
+
+{% for asset in site.static_files %}
+    {% if asset.path contains '/assets/images' or asset.path contains '/assets/posts' or asset.extname == '.js' %}
+    urlsToCache.push("{{ file.path }}")
+    {% endif %}
+{% endfor %}
+
+// Cache posts
+{% for post in site.posts %}
+  urlsToCache.push("{{ post.url }}")
+{% endfor %}
+
+// Cache pages
+{% for page in site.html_pages %}
+  urlsToCache.push("{{ page.url }}")
+{% endfor %}
 
 self.addEventListener('install', function(event) {
   // Perform install steps
