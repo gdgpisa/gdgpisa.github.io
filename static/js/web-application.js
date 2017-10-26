34f655bf2c5bdff8b7ba416b78e4901b70bd2958
domenicoblanco <doddo97@me.com>
2017-10-26 12:20:44 +0200

    Update web-application.js

--- static/js/web-application.js
+++ static/js/web-application.js
@@ -1,8 +1,12 @@
-if('serviceWorker' in navigator) {
-  navigator.serviceWorker.register('/sw.js').then(function(registration) {
-    // Registration was successful
-  }).catch(function(err) {
-    // registration failed :(
-    console.log('ServiceWorker registration failed: ', err);
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', function() {
+    navigator.serviceWorker.register('/sw.js').then(function(registration) {
+      // Registration was successful
+      console.log('ServiceWorker registration successful with scope: ', registration.scope);
+    }, function(err) {
+      // registration failed :(
+      console.log('ServiceWorker registration failed: ', err);
+      register();
+    });
   });
 }
