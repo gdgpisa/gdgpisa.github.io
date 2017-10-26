933eb5d31c3872125b0690a56d7ecba80152f0a8
domenicoblanco <doddo97@me.com>
2017-10-26 10:54:57 +0200

    Update web-application.js

--- static/js/web-application.js
+++ static/js/web-application.js
@@ -1,20 +1,8 @@
-function showOfflineToast() {
-	if (navigator.serviceWorker) {
-  		navigator.serviceWorker.register('/sw.js').then(function(reg) {
-      		if (!reg.installing) return;
-      		console.log("[*] ServiceWorker is installing...");
-       		var worker = reg.installing;
- 			worker.addEventListener('statechange', function() {
-          		if (worker.state == 'redundant') {
-              		console.log('[*] Install failed');
-          		}
-          		if (worker.state == 'installed') {
-              		console.log('[*] Install successful!');
-          		}
-          		if (worker.state == 'activated' && !navigator.serviceWorker.controller) {
-            		setInterval(showOfflineToast(),30);
-          		}
-      		});
-  		});
-	}
+if('serviceWorker' in navigator) {
+  navigator.serviceWorker.register('/sw.js').then(function(registration) {
+    // Registration was successful
+  }).catch(function(err) {
+    // registration failed :(
+    console.log('ServiceWorker registration failed: ', err);
+  });
 }
