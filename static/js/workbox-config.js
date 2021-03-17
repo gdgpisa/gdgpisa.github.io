af2d6e562fe83f21907a29944361b2cba89b13ec
domenicoblanco <doddo97@me.com>
2021-03-17 10:46:54 +0100

    Updated Service Worker to keep trigger the beforeinstallprompt event

---
+++ static/js/workbox-config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  "globDirectory": "_site/",
+  "globPatterns": [
+    "**/*.{html,xml,json,css,scss,md,eot,svg,ttf,woff,woff2,otf,js,png,jpg,webapp,jpeg,gif}"
+  ],
+  "swDest": "sw.js"
+};
